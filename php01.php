075530e35cfc9640f7f72a999e3230b0d3442886
sabbir hossain <mdsabbir477470@gmail.com>
2023-05-23 00:58:48 +0600

    php data type done

--- php01.php
+++ php01.php
@@ -1,2 +1,24 @@
 <?php
-echo "Hello World 11";
+$name = 'Sabbir Hossain';
+$age = 20;
+$qs = "Who are you?";
+$name1 = 'Hasin';
+$name2 = 'Hayder';
+
+// br tag er poribatta \n use kora best.
+echo "Hello I am {$name}. I am {$age}.\n";
+echo "{$qs}\n";
+
+var_dump($name1, $name2); // php type dackta var_dump()
+
+// php data type explore
+/*
+    1.Integer Int
+    2.Double / Float
+    3.Boolean
+    4.Null
+    5.String
+    6.Array
+    7.Object
+    8.Resource
+*/
