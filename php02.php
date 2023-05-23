2c270bb55a4a31f7c9b880cfc75168fd53688271
sabbir hossain <mdsabbir477470@gmail.com>
2023-05-23 13:17:11 +0600

    php number done

--- php02.php
+++ php02.php
@@ -4,3 +4,14 @@ $number = 12;
 $number += 13; // $number = $number + 13;
 
 echo $number;
+echo '<br>';
+
+$n = 12;
+$o = 017;
+$h = 0x1B;
+
+printf("The binary equvilent of %d is %b \n", 1212, 1212);
+printf("The hexadecimal equvilent of %d is %X \n", 1212, 1212);
+printf("The octal equvilent of %d is %o \n", 27, 27);
+
+printf("The octal equvilent of %d is %o \n", 65536, 65536);
